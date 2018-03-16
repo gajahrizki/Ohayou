77e5719cdb6a10b10377bd1fe0a679251d2008df
gajahrizki <gajahrizki@gmail.com>
2018-03-16 14:17:31 +0700

    Add files via upload

---
+++ jsTubes.js
@@ -0,0 +1,9 @@
+	function lampuOn(){
+	var Lampu = document.getElementById("lampu");
+	Lampu.src = "img\L\love2.png";
+	}
+
+	function lampuOff(){
+	var Lampu = document.getElementById("lampu");
+	Lampu.src = "https://www.w3schools.com/js/pic_bulboff.gif";
+	} 
